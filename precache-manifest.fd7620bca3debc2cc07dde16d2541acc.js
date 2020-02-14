586a9272ecff51ef1fceb35167c55d5cad7f03b1
pipsen <mail@pipsen.org>
2020-02-14 15:20:20 +0100

    Updates

---
+++ precache-manifest.fd7620bca3debc2cc07dde16d2541acc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60706a8da975484d881cd2fce25d7cf2",
+    "url": "/PonyMaze/index.html"
+  },
+  {
+    "revision": "e6c08c96b7f1465b49c0",
+    "url": "/PonyMaze/static/css/2.3ce71253.chunk.css"
+  },
+  {
+    "revision": "3db9631434be6b5d71a9",
+    "url": "/PonyMaze/static/css/main.2b7ba3a0.chunk.css"
+  },
+  {
+    "revision": "e6c08c96b7f1465b49c0",
+    "url": "/PonyMaze/static/js/2.c8fc97c9.chunk.js"
+  },
+  {
+    "revision": "3db9631434be6b5d71a9",
+    "url": "/PonyMaze/static/js/main.af1cb294.chunk.js"
+  },
+  {
+    "revision": "3cc83649ae780c85e35d",
+    "url": "/PonyMaze/static/js/runtime~main.2799de1a.js"
+  },
+  {
+    "revision": "9e97ef2bc6a09f431a0cb9d839802f94",
+    "url": "/PonyMaze/static/media/domokun.9e97ef2b.png"
+  }
+]);
